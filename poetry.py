06198a8320e6b6029a0b14acd7131a535d414e49
Gonzalo Birrueta <birruetabusiness@gmail.com>
2019-11-08 12:04:47 -0800

    added comments

--- poetry.py
+++ poetry.py
@@ -8,16 +8,20 @@ For we have some flax-golden tales to spin.
 Come in!
 Come in!"""
 
+#Split poem into seperate lines
 my_poem = poem.split("\n")
 
+#Custom Function (normal numbered lines)
 def number_lines(poem):
     for line in enumerate(poem,1):
         print(line)
 
+#Lines printed backwards function, uses enumerate method and starts counting at 1.
 def lines_printed_backwards(poem):
     for line_num, line in reversed(list(enumerate(my_poem,1))):
         print(line_num, line)
 
+#Lines printed at random function, uses random index but does not check for duplicates.
 def lines_printed_random(poem):
     random_lines = []
     for line in poem:
@@ -28,6 +32,7 @@ def lines_printed_random(poem):
         print(line)
 
 
+#Function Calls
 lines_printed_backwards(my_poem)
 print("")
 lines_printed_random(my_poem)
